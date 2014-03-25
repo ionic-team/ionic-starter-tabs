66151d2cf37e99d16e587b5eeca840c50037aeeb
Max Lynch <max@drifty.com>
2014-03-25 11:58:03 -0500

    Added linking example

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,10 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {
 })
 .controller('FriendsCtrl', function($scope) {
+  $scope.friends = [
+    {name: 'Scruff McGruff', id: 1},
+    {name: 'Tom Collins', id: 2}
+  ];
 })
 .controller('AccountCtrl', function($scope) {
 });
