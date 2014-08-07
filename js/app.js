4a97196392df84ed30107e478e69570aaa2f7514
Jesús Rodríguez Rodríguez <Foxandxss@gmail.com>
2014-08-07 23:44:55 +0200

    Add a missing semicolon
    Since you're into putting semicolons, I think that that one is missing.
    
    If your style is not to put it, ignore me then :P
    
    Cheers.

--- js/app.js
+++ js/app.js
@@ -75,7 +75,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           controller: 'AccountCtrl'
         }
       }
-    })
+    });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
