db4c458fbb424f13ca72f383174b6d40237c00ab
Max Lynch <max@drifty.com>
2014-12-11 12:04:59 -0600

    Updated

--- js/app.js
+++ js/app.js
@@ -48,6 +48,25 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       }
     })
 
+    .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
     .state('tab.friends', {
       url: '/friends',
       views: {
@@ -75,7 +94,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           controller: 'AccountCtrl'
         }
       }
-    })
+    });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,14 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {
 })
 
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
 .controller('FriendsCtrl', function($scope, Friends) {
   $scope.friends = Friends.all();
 })
@@ -12,4 +20,7 @@ angular.module('starter.controllers', [])
 })
 
 .controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
 });
