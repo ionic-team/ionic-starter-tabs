7a323f4d0c4128b44a730397e54709f20756b8c6
Max Lynch <max@drifty.com>
2014-12-11 13:37:47 -0600

    Swipe to delete

--- js/controllers.js
+++ js/controllers.js
@@ -5,6 +5,9 @@ angular.module('starter.controllers', [])
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
--- js/services.js
+++ js/services.js
@@ -16,9 +16,14 @@ angular.module('starter.services', [])
     all: function() {
       return chats;
     },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
     get: function(chatId) {
-      // Simple index lookup
-      return chats[chatId];
+      for(var i = 0; i < chats.length; i++) {
+        if(chats[i].id === parseInt(chatId)) { return chats[i]; }
+      }
+      return null;
     }
   }
 })
