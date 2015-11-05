4d62094cc2b6b48f3edea2c190d4387a9ae1106b
Max Lynch <max@drifty.com>
2015-11-05 14:52:26 -0600

    Fix status bar default

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
+      StatusBar.styleDefault();
     }
   });
 })
