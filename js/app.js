cca714e723bfc0b5860d6825e6a7dfd7bc809de5
Max Lynch <max@drifty.com>
2015-12-30 22:26:52 -0600

    Back to JS scrolling until next 1.2 release

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
+.config(function($ionicConfigProvider) {
+  $ionicConfigProvider.scrolling.jsScrolling(true);
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
